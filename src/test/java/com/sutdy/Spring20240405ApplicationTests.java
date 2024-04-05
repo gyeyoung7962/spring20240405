b883336aa6035da3e7d25bd002309f4c3d7a9196
gyeyoung7962 <gaeyoung7962@naver.com>
2024-04-05 11:38:37 +0900

    first commit

---
+++ src/test/java/com/sutdy/Spring20240405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20240405ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
