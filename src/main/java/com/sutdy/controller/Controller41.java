853897e4842377bbd056fbdc565a79472a8ef664
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 10:04:52 +0900

    /api/someurl2의 get,post 요청 핸들러 메소드 작성

--- src/main/java/com/sutdy/controller/Controller41.java
+++ src/main/java/com/sutdy/controller/Controller41.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class Controller41 {
 
-    @GetMapping("/api/somemurl")
+    @GetMapping("/api/someurl")
     public void method1() {
         System.out.println("Controller41.method1");
     }
@@ -15,6 +15,16 @@ public class Controller41 {
     @PostMapping("/api/someurl")
     public void method2() {
         System.out.println("Controller41.method2");
+    }
+
+    @GetMapping("/api/someurl2")
+    public void method3() {
+        System.out.println("Controller41.method3");
+    }
+
+    @PostMapping("/api/someurl2")
+    public void method4() {
+        System.out.println("Controller41.method4");
     }
 
 }
