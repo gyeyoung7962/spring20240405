183ee856ff1019e9683936c1919f4db6fedd4017
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 15:08:17 +0900

    JavaBean 객체 응답
    json으로 직렬화할때 get메소드로 프로퍼티가 결정됨

---
+++ src/main/java/com/sutdy/domain/MyBean412.java
@@ -0,0 +1,17 @@
+package com.sutdy.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MyBean412 {
+
+
+    private String name;
+    private Double height;
+    private MyBean411 info;
+    private List<String> foods;
+    private Boolean married;
+
+}
